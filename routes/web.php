66fca77ed0a8ba4df4fcd70d0a6e9faaf30a19a7
sara-sarsora <sara223101187@nmu.edu.eg>
2025-05-07 17:45:07 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -9,3 +9,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/posts', [PostController::class, 'index']);
